0311dd6c6578d709fa0261bc3118065290653fbb
shepard13 <yurets.andrey13@gmail.com>
2021-05-27 16:59:57 +0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: "MY-SITE",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "\u0016CFPAT-L7GDde7iQYZHARmzZrg2KvMroCARo-c-rC7oCa0rxYA",
+        spaceId: "6xlij974l04a",
+      },
+    },
+  ],
+};
